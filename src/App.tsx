1ca5c89f49d4beb21f004b48d0538842169da4a5
aleguztor <kihacaoficial@gmail.com>
2025-03-06 21:45:55 +0100

    fix with scroll

--- src/App.tsx
+++ src/App.tsx
@@ -88,7 +88,7 @@ function App() {
       <div
         style={{
           position: "relative",
-          width: "100vw",
+          width: "100%",
           display: "flex",
           alignItems: "center",
           justifyContent: "center",
